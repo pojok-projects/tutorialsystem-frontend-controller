66089219f8b37c7f58561affc145c8550172b52d
Suherman <suherman@qantas.com.au>
2019-09-05 04:38:14 +1000

    Align App.tsx with pull request.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar'
+import Wrapper from './components/container/Wrapper';
+import DrawerMenu from './components/drawer/Drawer'
+import VideoGllery from './components/video-gallery/VideoGallery'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tutorialinaja.tech (demo)
-          <br/>
-        Powered by &nbsp;
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ReactJS
-        </a>
-        </p>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    open: false
+  }
+
+  handleToggleMenu = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <Navbar onClickMenu={this.handleToggleMenu} />
+        <DrawerMenu open={this.state.open} onClickMenu={this.handleToggleMenu} />
+        <main>
+        <Route exact path="/" component={Wrapper} />
+        <Route path="/gallery" component={VideoGllery} />
+          {/* <Wrapper /> */}
+        </main>
+      </Router>
+    )
+  }
 }
-
-export default App;
