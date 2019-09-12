262bd917dd0ade21cd6c5b5e50cfe60e36acbfe6
Suherman <suherman@qantas.com.au>
2019-09-13 05:17:09 +1000

    Add default not found page, redirect index.html to  home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar/Navbar'
-import Wrapper from './components/container/Wrapper';
-import DrawerMenu from './components/drawer/Drawer'
-import VideoGllery from './components/video-gallery/VideoGallery'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Wrapper from "./components/container/Wrapper";
+import NotFound from "./components/container/NotFound";
+import DrawerMenu from "./components/drawer/Drawer";
+import VideoGallery from "./components/video-gallery/VideoGallery";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 export default class App extends React.Component {
   state = {
     open: false
-  }
+  };
 
   handleToggleMenu = () => {
-    this.setState({ open: !this.state.open })
-  }
-
+    this.setState({ open: !this.state.open });
+  };
 
   render() {
     return (
       <Router>
         <Navbar onClickMenu={this.handleToggleMenu} />
-        <DrawerMenu open={this.state.open} onClickMenu={this.handleToggleMenu} />
-        <main>
-            <Route exact path="/" component={Wrapper} />
-            <Route path="/gallery" component={VideoGllery} />
-        </main>
+        <DrawerMenu
+          open={this.state.open}
+          onClickMenu={this.handleToggleMenu}
+        />
+        <Switch>
+          <Route exact path="/" component={Wrapper} />
+          <Route path="/gallery" component={VideoGallery} />
+          <Redirect from="/index.html" to="/" />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </Router>
-    )
+    );
   }
 }
-
