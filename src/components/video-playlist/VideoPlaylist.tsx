39597985b9e0c7aed75829220a22c9e4005b99c1
agungjati <agungjati94@gmail.com>
2019-10-26 00:47:58 +0700

    perbaiki tampilan video playlist versi desktop

--- src/components/video-playlist/VideoPlaylist.tsx
+++ src/components/video-playlist/VideoPlaylist.tsx
@@ -1,146 +1,160 @@
 import React, {Component} from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Container from '@material-ui/core/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Alert from 'react-bootstrap/Alert';
-import Avatar from '@material-ui/core/Avatar';
+import { Container, Paper, Grid, Typography, LinearProgress, Button, Divider, Avatar } 
+  from '@material-ui/core';
 import './video-playlist.css';
 
 // Icon
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import Forward from '@material-ui/icons/Reply';
-import Comment from '@material-ui/icons/QuestionAnswer';
+import ForwardIcon from '@material-ui/icons/Reply';
+import CommentIcon from '@material-ui/icons/QuestionAnswer';
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 
 import {Link} from 'react-router-dom';
 
 export class VideoPlaylist extends Component {
+
   render() {
     return (
       <Container maxWidth="lg" className="wrapper">
-        <Card style={{borderRadius: '25px'}}>
-          <CardContent>
-            <Col xs={12}>
-              <Row>
-                <Col md={8} xs={12} className="watch-video pt-3">
-                  <video src="/assets/sample.mp4" controls></video>
-                </Col>
-                <ListVideo />
-              </Row>
-              <Row>
-                <Col md={8} xs={12} className="step-section">
-                  <div className="container-step">
-                    <ul>
-                      {[1, 2, , 3, 4, 5].map(res => (
-                        <li>{res}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <Row>
-                    <Col md={6}>
-                      <h2>Lorem Impsum</h2>
-                    </Col>
-                  </Row>
-                  <Row className="mt-2">
-                    <Col md={3}>120.000 Views</Col>
-                    <Col md={3}>Oktober, 8 2019</Col>
-                  </Row>
-                  <Row>
-                    <Col md={2} className="icons">
-                      <div className="icon red">
-                        <ThumbUpIcon fontSize="default" />
-                        <span>200</span>
-                      </div>
-                    </Col>
-                    <Col md={2} className="icons">
-                      <div className="icon">
-                        <ThumbDownIcon fontSize="default" />
-                        <span>200</span>
-                      </div>
-                    </Col>
-                    <Col md={{span: 2, offset: 1}}>
-                      <div className="icon2" id="forward">
-                        <Forward fontSize="default" />
-                      </div>
-                    </Col>
-                    <Col md={{span: 2}}>
-                      <div className="icon2">
-                        <Comment fontSize="default" />
-                      </div>
-                    </Col>
-                    <Col md={{span: 2}}>
-                      <div className="icon2">
-                        <PlaylistAddIcon fontSize="default" />
-                      </div>
-                    </Col>
-                    <Col md={{span: 1}}>
-                      <div id="dotthree">
-                        <MoreHorizIcon fontSize="large" />
-                      </div>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Col>
-          </CardContent>
-        </Card>
+        <Grid container justify="space-between">
+          <Grid item xs lg={8} >
+            <video style={{width: "100%"}} src="/assets/sample.mp4" controls></video>
+          </Grid>
+          <Grid item xs lg={4}>
+            <Playlist />      
+          </Grid>
+        </Grid>
+        <Grid container  direction="column" style={{marginTop: "15px"}}>
+          <Grid item lg={8}>
+            <VideoPlaylistToolBar />
+            <br />
+            <Divider />
+            <VideoPlaylistUser />
+            <br />
+            <Divider />
+          </Grid>
+        </Grid>
       </Container>
     );
   }
 }
 
-class ListVideo extends Component {
+class Playlist extends Component {
+
   render() {
-    return (
-      <Col md={4} xs={12} className="pt-3">
-        <Row>
-          <Col xs={12}>
-            <h5>
-              <b>Lorem ipsum dolor sit amet consectetur.</b>
-            </h5>
-            <br />
-          </Col>
-          <UserUpload />
-          <Col
-            xs={12}
-            style={{padding: '0', maxHeight: '270px', overflowY: 'scroll'}}
-          >
-            <Col xs={12}>
-              {[0, 1, 2, 3, 4, 5].map(value => (
-                <Alert key={value} variant="secondary" className="grid-video">
-                  <span className="text-secondary">
-                    <Link to="/view" className="video-gallery-title">
-                      Lorem, ipsum dolor. {value}
-                    </Link>
-                  </span>
-                </Alert>
-              ))}
-            </Col>
-          </Col>
-        </Row>
-      </Col>
+    return(
+    <Paper style={{marginLeft: "20px" }}>
+      <div className="playlist-category">
+          <Typography variant="h6">
+            Kusuka Pelangi di Awan
+          </Typography>
+          <Typography variant="body2">
+            Ini buatanku yang pertama
+          </Typography>
+      </div>
+      <Grid container direction="column" className="content-pl-item">
+        <Grid item>
+          <PlaylistItem />
+        </Grid>
+        <Grid item>
+          <PlaylistItem />
+        </Grid>
+        <Grid item>
+          <PlaylistItem />
+        </Grid>
+        <Grid item>
+          <PlaylistItem />
+        </Grid>
+        <Grid item>
+          <PlaylistItem />
+        </Grid>
+      </Grid>
+    </Paper>
     );
   }
 }
 
-function UserUpload() {
-  return (
-    <Col xs={12}>
-      <Row>
-        <Col xs={2}>
-          <Avatar alt="Remy Sharp" src="/assets/user-avatar.jpg" />
-        </Col>
-        <Col xs={10}>
-          <p>Lorem ipsum dolor sit.</p>
-        </Col>
-      </Row>
-      <br />
-    </Col>
-  );
+class PlaylistItem extends Component {
+
+  render() {
+    return(
+      <Grid container className="playlist-item">
+        <Grid item className="playlist-item-thumbnail">
+          <img src="/assets/recomended-thumbnail.jpg" width="100" alt="recomended-thumbnail.jpg" />
+          <LinearProgress variant="determinate" color="secondary" value={50} />
+          <div className="pl-i-duration">4.13</div>
+        </Grid>
+        <Grid item className="playlist-item-title">
+          <Typography variant="h6">
+            Kusuka Pelangi di Awan
+          </Typography>
+          <Typography variant="subtitle2">
+            Ini buatanku yang pertama
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+class VideoPlaylistToolBar extends Component {
+
+  render() {
+    return (
+        <Grid container  direction="column">
+            <Typography variant="h6">
+              Kusuka Pelangi di Awan Part 1
+            </Typography>
+          <Grid item>
+            <Typography variant="body2">
+              1.456.620 x ditonton • 28 Feb 2019
+            </Typography>
+          </Grid>
+          <Grid container className="pl-i-toolbar" justify="space-between" alignItems="center">
+            <div>
+              <Button size="small" className="red">
+                <ThumbUpIcon></ThumbUpIcon>&nbsp;<span>140</span>
+              </Button>
+              &nbsp;
+              <Button size="small">
+                <ThumbDownIcon></ThumbDownIcon>&nbsp;<span>140</span>
+              </Button>
+            </div>
+            <div>
+              <Button size="small">
+                <ForwardIcon></ForwardIcon>
+              </Button>
+              &nbsp;
+              <Button size="small">
+                <CommentIcon></CommentIcon>
+              </Button>
+              &nbsp;
+              <Button size="small">
+                <MoreHorizIcon></MoreHorizIcon>
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+    );
+  }
+}
+
+class VideoPlaylistUser  extends Component {
+
+  render() {
+    return (
+      <Grid container  justify="space-between" alignItems="center" style={{marginTop: "10px"}}>
+        <Avatar src="/assets/icon-user.png" alt="icon-user.png"  style={{marginRight: "10px"}}/>
+        <Grid item xs >
+          <Typography variant="h6">Pojokan</Typography>
+          <Typography variant="subtitle1">11,2 follower</Typography>
+        </Grid>
+        <Button className="btn-red" size="large" >Follow</Button>
+      </Grid>
+      )
+  }
 }
 
 export default VideoPlaylist;
