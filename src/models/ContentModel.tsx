253ef8513ddee5d3d541875f0e0a4d8949da6665
franky19 <frankimarbun@gmail.com>
2019-11-04 15:43:18 +0700

    update controller dan model

---
+++ src/models/ContentModel.tsx
@@ -0,0 +1,30 @@
+import { VideoModel } from "./VideoModel";
+
+
+// or category 
+export class MetaVideoModel {
+    public duration:VideoModel;
+    public size:VideoModel;
+    public file_path:VideoModel;
+    public file_name:VideoModel;
+    public format:VideoModel;
+    public resolution:VideoModel;
+    public id:"cb92f773-c0a9-41dc-8cc9-9be9d680097c";
+}
+
+export class ContentModel{
+    public comment:null;
+    public video_share:null;
+    public category_id:"2f0a68a1-115d-4c24-9173-404638a1fc27";
+    public dislikes: null;
+    public  video_downloads: null;
+    public  video_description: string="";
+    public video_title: string="";
+    public  user_id: "91fa1120-17d4-442b-8338-aa3ab7bbc14e";
+    public subtitle: null;
+    public video_genre: string="";
+    public id: "56b140a1-16a7-4fbb-b852-7365fbff7112";
+    public  video_saves: null;
+    public video_viewers: null;
+    public  likes: null
+}
