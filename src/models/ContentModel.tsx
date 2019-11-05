65c59179a780a8df0ac0cdf5890ee0ca727dbfbe
franky19 <frankimarbun@gmail.com>
2019-11-05 16:36:27 +0700

    perbaiki ContentModel

--- src/models/ContentModel.tsx
+++ src/models/ContentModel.tsx
@@ -4,19 +4,19 @@ import { VideoModel } from "./VideoModel";
 // or category 
 export class ContentModel{
     public comment:any=null;
-    public video_share:string="";
+    public video_share:any=null;
     public category_id:string="2f0a68a1-115d-4c24-9173-404638a1fc27";
-    public dislikes: number=0;
-    public  video_downloads: number=0;
+    public dislikes: any=null;
+    public  video_downloads: any=null;
     public  video_description: string="";
     public video_title: string="";
     public  user_id: string="91fa1120-17d4-442b-8338-aa3ab7bbc14e";
-    public subtitle: string="";
+    public subtitle: any=null;
     public video_genre: string="";
     public id: string="56b140a1-16a7-4fbb-b852-7365fbff7112";
-    public  video_saves:number=0;
-    public video_viewers: number=0;
-    public  likes: number=0;
+    public  video_saves:any=null;
+    public video_viewers: any=null;
+    public  likes: any=null;
 }
 export class MetaVideoModel{
     public duration:number=0;
